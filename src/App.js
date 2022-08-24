d17f5508f16c47f25387bab834660b347b8adfe7
Mridul47 <mrkoirala58@gmail.com>
2022-08-24 13:38:55 +0545

    fetching data creating json file and using it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Europeanglorys from './records.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <strong>...European Success</strong>
+      <br/>
+      {
+        Europeanglorys && Europeanglorys.map(record =>{
+          return(
+            <div className='box' key={record.id}>
+              <strong>{record.Name}</strong><br/>
+              {record.content} <br/>
+
+              {record.UCL && record.UCL.map(data=>{
+                return(
+                  <div key={record.id}>
+                    {data.titles}
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
